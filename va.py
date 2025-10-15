de4bb121b2c92f694149461935203bb4ea3abf7f
HuganAndersson <Hugo020910@hotmail.com>
2025-10-15 13:54:05 +0200

    Add files via upload

---
+++ va.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+
+
+
+
+class Balls():
+    def __init__(self):
+        pass
+        
+
+
+class Que():
+    pass
+
+
+def screen():
+    print(1)
+    window = tk.Tk()
+    greeting = tk.Label(text ="Hello, Tkinter")
+    greeting.pack()
+    window.mainloop()
+    print(2)
+    
+screen()
+
+
+
